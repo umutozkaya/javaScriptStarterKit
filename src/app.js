c453212afb94e2fbe1f2f76cc8575d4de86397cd
umutozkaya <74725599+umutozkaya@users.noreply.github.com>
2021-10-19 00:02:02 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun =9.30 
+
+
+console.log(dolarDun)
+
+let konutKredileri =["Emlak Kredisi","Taşıt Kredisi", "Kamu Konut Kredisi","Özel Konut Kredileri"]
+
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length; i++) {
+        console.log("<li>"+konutKredileri[i]+"</li>");
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
